777e69409582509b7b64b780ac7c9c3476ae5a72
ilya moskovko <ilya@moskovko.com>
2013-09-09 16:26:57 -0700

    added ability to process GPX file on server side and return array of coordinates in JSON to the client.

--- app/app.rb
+++ app/app.rb
@@ -1,23 +1,19 @@
 require 'nokogiri'
 require 'sinatra'
+require 'json'
 
 get '/' do
   erb :app
 end
 
 post '/' do
-  p params['gpxfile'][:filename]
   f = File.open(params['gpxfile'][:tempfile])
   doc = Nokogiri::XML(f)
   f.close
   lat = doc.xpath('//xmlns:trkseg//@lat').map {|pt| pt.value}
   lon = doc.xpath('//xmlns:trkseg//@lon').map {|pt| pt.value}
-  coors = lat.zip(lon)
-  puts "var path = ["
-  coors.each do |c|
-    puts "  new google.maps.LatLng(#{c[0]}, #{c[1]})#{',' if c != coors.last}"
-  end
-  puts "];";
+  coors = lat.zip(lon).map {|coor| {:latitude => coor[0], :longitude => coor[1]}}
+  coors.to_json
 end
 
 #f = File.open(ARGV[0])
